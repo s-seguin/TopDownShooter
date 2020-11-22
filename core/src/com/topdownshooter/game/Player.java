2e802fe8bab6175eb18b11b2a7b7a72d3e7bc740
Stuart Seguin <stuseguin@gmail.com>
2020-11-21 21:23:16 -0700

    fix: improve world collisions
    - remove bug that let players move through walls

--- core/src/com/topdownshooter/game/Player.java
+++ core/src/com/topdownshooter/game/Player.java
@@ -7,13 +7,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-
-class Direction {
-    public static final float up = 90;
-    public static final float down = 270;
-    public static final float left = 180;
-    public static final float right = 0;
-}
+import datastructures.Direction;
 
 public class Player {
     Vector2 position = new Vector2(0, 0);
@@ -67,12 +61,6 @@ public class Player {
         handleCollisions(velocity);
         position.x += velocity.x;
         position.y += velocity.y;
-//        boolean isColliding = isColliding();
-//        System.out.println("isColling " + isColliding);
-//        if (isColliding) {
-//            System.out.println("here");
-//            position = prevPosition;
-//        }
 
         constrainToMap();
     }
@@ -84,128 +72,50 @@ public class Player {
     private void handleCollisions(Vector2 velocity) {
         TiledMapTileLayer collisionLayer = (TiledMapTileLayer) map.tiledMap.getLayers().get("house");
 
-        float topEdge = sprite.getBoundingRectangle().y + sprite.getBoundingRectangle().height;
-        float bottomEdge = sprite.getBoundingRectangle().y;
+        velocity.x = (velocity.x > 0) ?
+                handleCollisionRight(collisionLayer, velocity.x) :
+                handleCollisionLeft(collisionLayer, velocity.x);
+        velocity.y = (velocity.y > 0) ?
+                handleCollisionUp(collisionLayer, velocity.y) :
+                handleCollisionDown(collisionLayer, velocity.y);
+    }
+
+    private float handleCollisionRight(TiledMapTileLayer collisionLayer, float xVelocity) {
         float rightEdge = sprite.getBoundingRectangle().x + sprite.getBoundingRectangle().width;
-        float leftEdge = sprite.getBoundingRectangle().x;
 
-//        printMap(collisionLayer);
+        int col = (int) Math.ceil(position.x / collisionLayer.getTileWidth());
+        int row = Math.round(position.y / collisionLayer.getTileHeight());
 
-        // x collisions
-
-        //which row of tiles intersect with bottom edge and topEdge
-        int topRow = (int) Math.floor(topEdge / collisionLayer.getTileHeight());
-        int bottomRow = (int) Math.ceil(bottomEdge / collisionLayer.getTileHeight());
-
-        if (velocity.x > 0) {
-            //moving right
-            System.out.println("moving right");
-
-            int startingCol = (int) Math.floor(rightEdge / collisionLayer.getTileWidth());
-
-            // find closest impassable object
-            // for intersecting rows
-            for (int row = bottomRow - 1; row < topRow; row++) {
-                for (int col = startingCol; col < collisionLayer.getWidth(); col++) {
-                    TiledMapTileLayer.Cell cell = collisionLayer.getCell(col, row);
-
-                    if (cell != null) {
-                        float distanceToClosestObj = (col * collisionLayer.getTileWidth() - rightEdge);
-                        velocity.x = Math.min(velocity.x, distanceToClosestObj);
-                        return;
-                    }
-
-                }
-            }
-
-        } else if (velocity.x < 0) {
-            System.out.println("moving left");
-
-            int startingCol = (int) Math.ceil(leftEdge / collisionLayer.getTileWidth());
-
-            // find closest impassable object
-            // for intersecting rows
-            for (int row = bottomRow - 1; row < topRow; row++) {
-                for (int col = startingCol; col >= 0; col--) {
-                    TiledMapTileLayer.Cell cell = collisionLayer.getCell(col, row);
-
-                    if (cell != null) {
-                        float distanceToClosestObj = leftEdge - (col * collisionLayer.getTileWidth() + collisionLayer.getTileWidth());
-                        velocity.x = -Math.min(Math.abs(velocity.x), Math.abs(distanceToClosestObj));
-                        return;
-                    }
-
-                }
-            }
-        }
-
-        // y collisions
-        int leftCol = (int) Math.floor(leftEdge / collisionLayer.getTileWidth());
-        int rightCol = (int) Math.ceil(rightEdge / collisionLayer.getTileWidth());
-
-        if (velocity.y > 0) {
-
-            System.out.println("moving up");
-
-            int startingRow = (int) Math.floor(topEdge / collisionLayer.getTileHeight());
-            for (int col = leftCol - 1; col < rightCol; col++) {
-                System.out.printf("Col %d leftCol %d rightCol %d topEdge %f%n", col, leftCol - 1, rightCol, topEdge);
-                for (int row = startingRow; row <= collisionLayer.getHeight(); row++) {
-                    System.out.printf("Row %d%n", row);
-                    TiledMapTileLayer.Cell cell = collisionLayer.getCell(col, row);
-
-                    if (cell != null) {
-                        float distanceToClosestObj = (row * collisionLayer.getTileHeight()) - topEdge;
-                        velocity.y = Math.min(Math.abs(velocity.y), Math.abs(distanceToClosestObj));
-                        return;
-                    }
-                }
-            }
-        } else if (velocity.y < 0) {
-
-            System.out.println("moving down");
-
-            int startingRow = (int) Math.floor(bottomEdge / collisionLayer.getTileHeight());
-            for (int col = leftCol - 1; col < rightCol; col++) {
-                System.out.printf("Col %d leftCol %d rightCol %d topEdge %f%n", col, leftCol - 1, rightCol, topEdge);
-                for (int row = startingRow; row >= 0; row--) {
-                    System.out.printf("Row %d%n", row);
-                    TiledMapTileLayer.Cell cell = collisionLayer.getCell(col, row);
-
-                    if (cell != null) {
-                        float distanceToClosestObj = bottomEdge - (row * collisionLayer.getTileHeight() + collisionLayer.getTileHeight());
-                        velocity.y = -Math.min(Math.abs(velocity.y), Math.abs(distanceToClosestObj));
-                        return;
-                    }
-                }
-            }
-        }
+        return handleCollision(col, row, collisionLayer, (col * collisionLayer.getTileWidth() - rightEdge), xVelocity);
     }
 
-    private void printMap(TiledMapTileLayer collisionLayer) {
-        float playerPosX = Math.round(position.x / collisionLayer.getTileWidth());
-        float playerPosY = Math.round(position.y / collisionLayer.getTileHeight());
+    private float handleCollisionLeft(TiledMapTileLayer collisionLayer, float xVelocity) {
+        int col = (int) Math.floor(position.x / collisionLayer.getTileWidth());
+        int row = Math.round(position.y / collisionLayer.getTileHeight());
 
-        for (int row = 0; row < collisionLayer.getWidth(); row++) {
-            for (int col = 0; col < collisionLayer.getHeight(); col++) {
-                TiledMapTileLayer.Cell cell = collisionLayer.getCell(col, row);
-                char c = '_';
-                if (playerPosX == col && playerPosY == row && cell != null) {
-                    c = '*';
-                } else if (playerPosX == col && playerPosY == row) {
-                    c = 'P';
-                } else if (cell != null) {
-                    c = 'X';
-                }
-                System.out.print(c + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.println();
-        System.out.println();
+        return handleCollision(col, row, collisionLayer, 0, xVelocity);
     }
 
+    private float handleCollisionUp(TiledMapTileLayer collisionLayer, float yVelocity) {
+        float topEdge = sprite.getBoundingRectangle().y + sprite.getBoundingRectangle().height;
+
+        int col = Math.round(position.x / collisionLayer.getTileWidth());
+        int row = (int) Math.ceil(position.y / collisionLayer.getTileHeight());
+
+        return handleCollision(col, row, collisionLayer, (row * collisionLayer.getTileHeight() - topEdge), yVelocity);
+
+    }
+
+    private float handleCollisionDown(TiledMapTileLayer collisionLayer, float yVelocity) {
+        int col = Math.round(position.x / collisionLayer.getTileWidth());
+        int row = (int) Math.floor(position.y / collisionLayer.getTileHeight());
+
+        return handleCollision(col, row, collisionLayer, 0, yVelocity);
+    }
+
+    private float handleCollision(int col, int row, TiledMapTileLayer collisionLayer, float updatedVelocity, float originalVelocity) {
+        return collisionLayer.getCell(col, row) != null ? updatedVelocity : originalVelocity;
+    }
 
     private void constrainToMap() {
         if (position.x < 0) {
---
+++ core/src/datastructures/Direction.java
@@ -0,0 +1,8 @@
+package datastructures;
+
+public class Direction {
+    public static final float up = 90;
+    public static final float down = 270;
+    public static final float left = 180;
+    public static final float right = 0;
+}
